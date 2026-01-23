5f11b56342e9ba29afb4e80c1b66e8e189276e96
Jin Liu <z5596858@ad.unsw.edu.au>
2026-01-24 00:14:00 +1100

    Initial commit: backend + frontend scaffold

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+};
